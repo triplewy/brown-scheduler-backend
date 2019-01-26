e6edf660855866d9fff4b2f5c45e0f20750f4b57
sch3 <steven.c.ho@gmail.com>
2019-01-26 13:50:16 -0500

    Add env to gitignore. Added mongo integration.

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+require('dotenv').config();
 const express = require('express');
+
 const http = require('http');
 const https = require('https');
 const bodyParser = require('body-parser');
 const app = express();
 const port =(process.env.PORT || 8090);
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://user-1:'+process.env.MONGOPW+'@brown-cluster-oqrxu.mongodb.net/test?retryWrites=true';
+const dbName = 'brownDB';
+const assert = require('assert');
 
 app.get('/status', function(req, res) {
-    res.json({
-        message: 'Backend Listening!',
-        code: 'status_endpoint'
-    })
+    MongoClient.connect(url, function(err, client) {
+      assert.equal(null, err);
+      console.log("Connected successfully to server");
+
+      const db = client.db(dbName);
+
+      db.collection("courses").findOne({}, function(err,result) {
+        console.log(result)
+        client.close();
+        res.json({
+            message: 'Backend Listening!',
+            code: 'status_endpoint'
+        })
+      });
+        
+    });
+    
 });
 app.set('port', port);
 
